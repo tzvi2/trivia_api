71faa963bcbf8be77024fa20cc5cb25d16d00ccd
T <tzvib8@gmail.com>
2022-05-26 17:06:31 -0400

    api spec

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const app = require('express')()
+const express = require('express')
+const path = require('path')
+
 let port = process.env.PORT || 3000
 
+const data = require('./questions.json')
+
+app.set('json spaces', 2)
+
+const options = {}
+
+
+app.use(express.static("public"))
+
 app.get('/', (req, res) => {
-    res.send('Yoghurt! bliyb 3')
+    res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.get('/bois', (req, res) => {
-    res.send({
-        "hi": "hellp"
-    })
+app.get('/questions/:topic/:number', (req, res) => {
+    let questions = []
+    
+    if (req.params.number > data[req.params.topic].length) {
+        res.send(`too many questions, please specify up to ${data[req.params.topic].length}`)
+        return
+    } else if (!data[req.params.topic]) {
+        res.send(`Topic not found`)
+    }
+    let numberOfQuestions = 0
+    usedIndexes = new Set()
+
+    while (numberOfQuestions < req.params.number) {
+        let random = Math.floor(Math.random() * data[req.params.topic].length)
+        while (usedIndexes.has(random)) {
+            random = Math.floor(Math.random() * data[req.params.topic].length)
+        }
+
+        questions.push(data[req.params.topic][random])
+        usedIndexes.add(random)
+        numberOfQuestions++
+    }
+    res.send(questions)
 })
 
 app.listen(port, () => {
